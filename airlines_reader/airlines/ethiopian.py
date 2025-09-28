1880a9953cfb89cc822f44fd5f283666725503f4
Vignesh KP <vigneshkp@microsoft.com>
2025-09-28 15:51:07 +0530

    Adding addional log level

--- airlines_reader/airlines/ethiopian.py
+++ airlines_reader/airlines/ethiopian.py
@@ -1,13 +1,11 @@
-
 import requests
 import logging
 import config
-
 from requests import HTTPError
-
 from airlines_reader.utils.cache import file_cache
 
 _LOG = logging.getLogger(__name__)
+_LOG.setLevel(config.HTTP_LOG_LEVEL)  # Ensure high verbosity
 
 _URL = "https://dxbooking.ethiopianairlines.com/api/graphql"
 
@@ -17,7 +15,8 @@ _HEADERS = {
     'Sec-Ch-Ua-Platform': '"macOS"',
     'Authorization': 'Bearer Basic anNvbl91c2VyOmpzb25fcGFzc3dvcmQ=',
     'Execution': '',
-    'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"', 'Sec-Ch-Ua-Mobile': '?0',
+    'Sec-Ch-Ua': '"Chromium";v="131", "Not_A Brand";v="24"',
+    'Sec-Ch-Ua-Mobile': '?0',
     'Adrum': 'isAjax:true',
     'Accept': '*/*',
     'Content-Type': 'application/json',
@@ -59,17 +58,57 @@ def _payload(pnr, last_name):
     }
 
 
+def _log_request(prepared):
+    """Logs the full HTTP request."""
+    _LOG.debug("----- HTTP Request -----")
+    _LOG.debug(f"{prepared.method} {prepared.url}")
+    for k, v in prepared.headers.items():
+        _LOG.debug(f"{k}: {v}")
+    if prepared.body:
+        try:
+            _LOG.debug("Body:\n%s", prepared.body.decode() if isinstance(prepared.body, bytes) else prepared.body)
+        except Exception:
+            _LOG.debug("Body: <non-decodable binary>")
+    _LOG.debug("------------------------")
+
+
+def _log_response(response):
+    """Logs the full HTTP response."""
+    _LOG.debug("----- HTTP Response -----")
+    _LOG.debug(f"Status: {response.status_code} {response.reason}")
+    for k, v in response.headers.items():
+        _LOG.debug(f"{k}: {v}")
+    try:
+        _LOG.debug("Body:\n%s", response.text)
+    except Exception:
+        _LOG.debug("Body: <non-decodable>")
+    _LOG.debug("-------------------------")
+    return response
+
+
 # @file_cache()
 def booking_details(pnr, last_name):
     _LOG.info(f"Retrieving booking details for pnr={pnr} last_name={last_name}")
     headers = config.HEADERS | _HEADERS
-    response = requests.post(_URL, json=_payload(pnr, last_name), headers=headers)
-    _LOG.info(f"Received response for pnr={pnr} last_name={last_name}: " + str(response))
+
+    session = requests.Session()
+    request = requests.Request("POST", _URL, json=_payload(pnr, last_name), headers=headers)
+    prepared = session.prepare_request(request)
+
+    _log_request(prepared)
+
+    response = session.send(prepared)
+    _log_response(response)
+
+    _LOG.info(f"Received response for pnr={pnr} last_name={last_name}: {response}")
     response.raise_for_status()
+
     json_resp = response.json()
-    _LOG.info("Response data: " + str(json_resp))
+    _LOG.info("Response data: %s", json_resp)
+
     if json_resp.get('data', {}).get('getMYBTripDetails'):
         return json_resp
+
     error_details = json_resp.get('extensions', {}).get('errors')
     _LOG.error(f"Received error for pnr={pnr}; last_name={last_name}; error_details={error_details}")
     raise HTTPError(error_details)
